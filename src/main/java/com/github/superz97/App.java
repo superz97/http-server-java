88aba55b59bf124b8cf3895bc123fcb898a02a2d
Zed <b.zangar15@gmail.com>
2026-08-09 16:29:58 +0500

    extract url path

--- src/main/java/com/github/superz97/App.java
+++ src/main/java/com/github/superz97/App.java
@@ -1,6 +1,8 @@
 package com.github.superz97;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,8 +17,23 @@ public class App {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
 
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+
+                String requestLine = bufferedReader.readLine();
+
                 OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+
+                if (requestLine != null && !requestLine.isEmpty()) {
+                    String[] requestPaths = requestLine.split(" ");
+
+                    String path = requestPaths[1];
+
+                    if (path.equals("/")) {
+                        outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    } else {
+                        outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    }
+                }
                 clientSocket.close();
             }
         } catch (IOException e) {
