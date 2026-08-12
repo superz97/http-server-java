71c4d2052fd947169b85e037e245da162c9c1655
Zed <b.zangar15@gmail.com>
2026-08-12 21:19:35 +0500

    read header

--- src/main/java/com/github/superz97/App.java
+++ src/main/java/com/github/superz97/App.java
@@ -7,6 +7,8 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main( String[] args ) {
@@ -27,6 +29,16 @@ public class App {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
         String requestLine = bufferedReader.readLine();
 
+        Map<String, String> headers = new HashMap<>();
+        while (true) {
+            String headerLine = bufferedReader.readLine();
+            if (headerLine == null || headerLine.isEmpty()) {
+                break;
+            }
+            String[] parts = headerLine.split(": ");
+            headers.put(parts[0].toLowerCase(), parts[1]);
+        }
+
         OutputStream outputStream = clientSocket.getOutputStream();
 
         if (requestLine != null && !requestLine.isEmpty()) {
@@ -40,6 +52,11 @@ public class App {
                 String body = path.substring("/echo/".length());
                 String response = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" +
                         "Content-Length: " + body.getBytes(StandardCharsets.UTF_8).length + "\r\n" + "\r\n" + body;
+                outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+            } else if (path.equals("/user-agent")) {
+                String userAgent = headers.get("user-agent");
+                String response = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" +
+                        "Content-Length: " + userAgent.getBytes(StandardCharsets.UTF_8).length + "\r\n" + "\r\n" + userAgent;
                 outputStream.write(response.getBytes(StandardCharsets.UTF_8));
             } else {
                 outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
